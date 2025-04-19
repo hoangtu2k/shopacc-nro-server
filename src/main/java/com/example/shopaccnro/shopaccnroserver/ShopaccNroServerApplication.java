52c2c8664b5ffff586a023b0d683464f9d916c0e
Phung Hoang Tu <thocodeonline@gmail.com>
2025-04-19 20:41:38 +0700

    create server + depe + setting application

---
+++ src/main/java/com/example/shopaccnro/shopaccnroserver/ShopaccNroServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopaccnro.shopaccnroserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopaccNroServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopaccNroServerApplication.class, args);
+    }
+
+}
